bbc02ebe7037251915dc898a0a1e5de4948e1ee5
judisuhu1-ux <judisuhu1@gmail.com>
2026-07-25 21:20:50 +0800

    Update link.js

--- link.js
+++ link.js
@@ -1,4 +1,6 @@
 function link() {location.replace("/");}
+function linksobet88() {location.replace("https://changelink.online/gdsbobet");}
+//
 function linkqqjago() {location.replace("https://www.qqjago.id/");}
 function linkalibababet() {location.replace("https://rebrand.ly/f4c4ba/");}
 function linkmagnum188() {location.replace("https://jali.me/magnum188-link");}
